d5152eadf4258aea494799bfb6fdfeca4c7368b0
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-08-21 09:52:05 +0530

    Infix to Postfix

---
+++ problems/infix2postfix.py
@@ -0,0 +1,25 @@
+def infix_to_postfix(expression):
+
+    stack = [] 
+    Priority = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    output = '' 
+    for c in expression:
+        if c.isalnum():
+            output += c
+        elif c == '(':
+            stack.append(c)
+        elif c == ')':
+            while stack and stack[-1] != '(':
+                output += stack.pop()
+            if stack and stack[-1] == '(':
+                stack.pop()
+        elif c in Priority:
+            while stack and stack[-1] != '(' and Priority.get(stack[-1], 0) >= Priority[c]:
+                output += stack.pop()
+            stack.append(c)
+    while stack:
+        output += stack.pop()
+    return output
+
+s = "a+b*(c^d-e)^(f+g*h)-i"
+print(infix_to_postfix(s))
